517a4dd1522921adff745e96252b93fcaa63f1ce
jhonatact <jhonatatiene118@gmail.com>
2025-07-16 15:34:47 -0300

    Implementando os metodos criar, buscarTodos e buscarPorId

--- src/produtos/produtos.service.ts
+++ src/produtos/produtos.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Produto } from './produto.entity';
 
 @Injectable()
-export class ProdutosService {}
+export class ProdutosService {
+    constructor(@InjectRepository(Produto) private readonly produtoRepo: Repository<Produto>) { }
+
+    async criar(produto: Partial<Produto>): Promise<Produto> {
+        return this.produtoRepo.save(produto)
+    }
+    
+    async buscarTodos(): Promise<Produto[]> {
+        return this.produtoRepo.find()
+    }
+
+    async buscarPorId(id: number): Promise<Produto | null> {
+        const resultado = await this.produtoRepo.findOneBy({ id });
+        if (!resultado) {
+            throw new NotFoundException(`Produto com ID ${id} não encontrado`)
+        }
+        return resultado
+    }
+}
