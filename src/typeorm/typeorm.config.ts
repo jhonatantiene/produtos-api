decfff995eceea44f6c2c373808508e158af9e13
jhonatact <jhonatatiene118@gmail.com>
2025-07-17 16:22:28 -0300

    Adicionando .env no projeto

--- src/typeorm/typeorm.config.ts
+++ src/typeorm/typeorm.config.ts
@@ -3,11 +3,11 @@ import { Produto } from "src/produtos/produto.entity"
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'mysql',
-    host: 'tramway.proxy.rlwy.net',
-    port: 58828,
-    username: 'root',
-    password: 'MWraNJOmVcCjmWUsIcIwfBqrAdEUByJi',
-    database: 'railway',
+    host: process.env.DB_HOST!,
+    port: parseInt(process.env.DB_PORT!),
+    username: process.env.DB_USERNAME!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
     entities: [Produto],
     autoLoadEntities: true,
     synchronize: true
