3316b26e79c813780de3f3801e121a3ca79866cd
jhonatact <jhonatatiene118@gmail.com>
2025-07-24 09:23:34 -0300

    .

--- src/produtos/produtos.controller.ts
+++ src/produtos/produtos.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { ProdutosService } from './produtos.service';
 import { Produto } from './produto.entity';
 import { CreateProdutoDto } from './dto/criar-produto.dto';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('produtos')
 export class ProdutosController {
     constructor(private readonly produtosService: ProdutosService) { }
 
     @Post('cadastrar')
-    criar(@Body() produto: CreateProdutoDto): Promise<Produto> {
+    @UseInterceptors(FilesInterceptor('imagem'))
+    criar(@UploadedFiles() imagem: Array<Express.Multer.File>, @Body() produto: CreateProdutoDto): Promise<Produto> {
+        console.log(imagem)
         return this.produtosService.criar(produto)
     }
 
